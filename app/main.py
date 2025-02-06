c66bb9e1a1d5e9906fd956b5d2832a3ec30ee64d
Bohdan Mykyta <justkexitmail@gmail.com>
2025-02-06 17:41:10 +0200

    first attempt

--- app/main.py
+++ app/main.py
@@ -1,13 +1,34 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return { "line": key[2],
+        "column": key[3],
+        "message": key[4],
+        "name": key[0],
+        "source": "flake8"
+        for key in error
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [format_linter_error(argument)],
+        "path": file_path
+        for argument in errors
+        if errors "status": "failed"
+        else "status": "passed"
+    }
+
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {
+        "errors": [format_linter_error(desc)],
+        "path": way
+        for way, desc in argument
+        if "errors" "status": "failed"
+        else "status": "passed"
+        }
+        for argument in linter_report
+        ]
+
+return
